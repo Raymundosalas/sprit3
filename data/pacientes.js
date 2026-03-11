0719b6f56135d74c9347adc31d394cf7badc6818
Raymundosalas <128091595+Raymundosalas@users.noreply.github.com>
2026-03-11 12:40:33 -0600

    Create pacientes.js

---
+++ data/pacientes.js
@@ -0,0 +1,18 @@
+let pacientes = [
+    {
+        id: 1,
+        nombre: "Juan Pérez",
+        telefono: "3312345678",
+        correo: "juan@email.com",
+        observaciones: "Paciente nuevo"
+    },
+    {
+        id: 2,
+        nombre: "María López",
+        telefono: "3323456789",
+        correo: "maria@email.com",
+        observaciones: "Control mensual"
+    }
+];
+
+module.exports = pacientes;
